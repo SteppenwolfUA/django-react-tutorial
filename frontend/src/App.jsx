0b6f02cc3894bd703226d0ae57c94067e406c08d
Volodymyr Goncharenko <movy.ukraine@gmail.com>
2024-10-09 17:36:06 +0200

    initial commit of my own project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-import react from "react"
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Home from "./pages/Home"
-import NotFound from "./pages/NotFound"
-import ProtectedRoute from "./components/ProtectedRoute"
+import react from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function Logout() {
-  localStorage.clear()
-  return <Navigate to="/login" />
+  localStorage.clear();
+  return <Navigate to='/login' />;
 }
 
 function RegisterAndLogout() {
-  localStorage.clear()
-  return <Register />
+  // if someone is registering we clear the local storage
+  // so they don't end up submitting access tokens to the register route
+  // where we potentially get an error
+
+  localStorage.clear();
+  return <Register />;
 }
 
 function App() {
@@ -21,20 +25,21 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route
-          path="/"
+          path='/'
           element={
             <ProtectedRoute>
               <Home />
             </ProtectedRoute>
           }
         />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<RegisterAndLogout />} />
-        <Route path="*" element={<NotFound />}></Route>
+
+        <Route path='/login' element={<Login />} />
+        <Route path='/logout' element={<Logout />} />
+        <Route path='/register' element={<RegisterAndLogout />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,23 +1,26 @@
-import axios from "axios";
-import { ACCESS_TOKEN } from "./constants";
-
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+import axios from "axios"
+import { ACCESS_TOKEN } from "./constants"
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
-});
+    baseURL: import.meta.env.VITE_API_URL // this allows us to import anything specified inside an environment variable file 
+    // if we want to have an environment variable loaded in our JavaScript /React code 
+    // it needs to start with VITE
+    // the idea here is that we're going to have this in an environment variable
+    // that it's really easy for us to load and change what the URL should be
+})
 
 api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem(ACCESS_TOKEN);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
-export default api;
+
+export default api
